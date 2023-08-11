5426f91fe4ede3eb1437ad10e52f8ff30644b54e
Hilal Özhan <hilalozhan0102@gmail.com>
2023-08-11 15:20:29 +0300

    Test3 python

---
+++ test3.py
@@ -0,0 +1,31 @@
+my_list=[1,2]
+
+my_list.append (5)
+my_list.append(8)
+#.append is adding value in list in order
+
+print(my_list[0]+my_list[3])
+ # output is "9"
+
+name="Luna"
+
+print("hello my name is %s"%name)
+ #output is "hello my name is Luna"
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+#output is "hellohellohellohellohellohellohellohellohellohello"
+
+text="hello my name is luna"
+harfler = list(text)
+print(harfler)
+count=0
+
+for x in harfler:
+    if x=='a':
+            count+=1
+
+print("a harfinden",count,"adet var")
+
+#output is "a harfinden 2 adet var"
