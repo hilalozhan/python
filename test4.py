84f4a1b77d1f1f22418c9b6a3a98327898b38c39
Hilal Özhan <hilalozhan0102@gmail.com>
2023-08-14 14:44:49 +0300

    Letter Program

--- test4.py
+++ test4.py
@@ -10,3 +10,18 @@ for letter in  letters:
 for letter ,n in count.items():
     print(letter," tekrar sayısı : ",n)
 
+'''
+output is :
+    h  tekrar sayısı :  2
+    e  tekrar sayısı :  2
+    l  tekrar sayısı :  4
+    o  tekrar sayısı :  1
+       tekrar sayısı :  4
+    m  tekrar sayısı :  2
+    y  tekrar sayısı :  1
+    n  tekrar sayısı :  1
+    a  tekrar sayısı :  2
+    i  tekrar sayısı :  2
+    s  tekrar sayısı :  1
+
+ '''  
