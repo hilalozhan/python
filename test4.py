8de7445b1dc9451ee0d43f8c10d1afc43f7330ae
Hilal Özhan <hilalozhan0102@gmail.com>
2023-08-14 14:41:51 +0300

    Letter Program

--- test4.py
+++ test4.py
@@ -1,2 +1,12 @@
-hello first branch try file
+text="hello my name is hilal"
+letters=list(text)
+count={}
+for letter in  letters:
+    if letter in count:
+        count[letter]+=1
+    else :
+        count[letter]=1
+
+for letter ,n in count.items():
+    print(letter," tekrar sayısı : ",n)
 
